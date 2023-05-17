9451a90d3512d7072c21f89a3e0d225b20373744
Ankush Agnihotri <ankush1802@outlook.com>
2023-05-17 17:47:00 +0530

    Code Checked In

---
+++ Controllers/BarcodeController.cs
@@ -0,0 +1,44 @@
+﻿using Barcode.Models;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using static QRCoder.PayloadGenerator;
+
+namespace Barcode.Controllers
+{
+    public class BarcodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            QRCodeModel model = new();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(QRCodeModel model)
+        {
+            Payload? payload = null;
+            switch (model.QRCodeType)
+            {
+                case 1: // compose sms
+                    payload = new SMS(model.SMSPhoneNumber, model.SMSBody);
+                    break;
+                case 2: // compose whatsapp message
+                    payload = new WhatsAppMessage(model.WhatsAppNumber, model.WhatsAppMessage);
+                    break;
+                case 3: //compose email
+                    payload = new Mail(model.ReceiverEmailAddress, model.EmailSubject, model.EmailMessage);
+                    break;
+                case 4: // wifi qr code
+                    payload = new WiFi(model.WIFIName, model.WIFIPassword, WiFi.Authentication.WPA);
+                    break;
+            }
+
+            QRCodeGenerator qrGenerator = new();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload);
+            BitmapByteQRCode qrCode = new(qrCodeData);
+            string base64String = Convert.ToBase64String(qrCode.GetGraphic(20));
+            model.QRImageURL = "data:image/png;base64," + base64String;
+            return View("Index", model);
+        }
+    }
+}
---
+++ Models/QRCodeModel.cs
@@ -0,0 +1,66 @@
+﻿namespace Barcode.Models
+{
+    public class QRCodeModel
+    {
+        public int QRCodeType
+        {
+            get;
+            set;
+        }
+        public string QRImageURL
+        {
+            get;
+            set;
+        } = string.Empty;
+
+        // for email qr codes
+        public string ReceiverEmailAddress
+        {
+            get;
+            set;
+        } = string.Empty;
+        public string EmailSubject
+        {
+            get;
+            set;
+        } = string.Empty;
+        public string EmailMessage
+        {
+            get;
+            set;
+        } = string.Empty;
+        //for sms qr codes
+        public string SMSPhoneNumber
+        {
+            get;
+            set;
+        } = string.Empty;
+        public string SMSBody
+        {
+            get;
+            set;
+        } = string.Empty;
+        // for whatsapp qr message code
+        public string WhatsAppNumber
+        {
+            get;
+            set;
+        } = string.Empty;
+        public string WhatsAppMessage
+        {
+            get;
+            set;
+        } = string.Empty;
+        // for wifi qr code
+        public string WIFIName
+        {
+            get;
+            set;
+        } = string.Empty;
+        public string WIFIPassword
+        {
+            get;
+            set;
+        } = string.Empty;
+    }
+}
